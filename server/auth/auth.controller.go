c25803417e16ba7f2e0ac4b11e7feba3a8a5e448
LJS360d <luca.lencioni09@gmail.com>
2025-01-04 15:14:42 +0100

    add guilds to /auth/@me

--- server/auth/auth.controller.go
+++ server/auth/auth.controller.go
@@ -26,8 +26,23 @@ func (s *AuthController) GetUser(c *gin.Context) {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
+	var guilds []string
+	// Loop through all cached guilds
+	for _, guild := range s.ds.State.Guilds {
+		_, err := s.ds.State.Member(guild.ID, user.ID)
+		if err != nil {
+			_, err = s.ds.GuildMember(guild.ID, user.ID)
+			if err != nil {
+				// User is not a member of this guild
+				continue
+			}
+		}
+		guilds = append(guilds, guild.ID)
+	}
+
 	c.JSON(200, gin.H{
 		"user":     user,
 		"is_owner": slices.Contains(config.OwnerIDs, user.ID),
+		"guilds":   guilds,
 	})
 }
