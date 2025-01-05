3f49584db313ad5ed1ea25b9b4a3404e3e1d1898
LJS360d <luca.lencioni09@gmail.com>
2025-01-05 15:21:21 +0100

    minor memory footprint optimizations

--- cmd/model/media-storage.go
+++ cmd/model/media-storage.go
@@ -7,148 +7,159 @@ import (
 	"rolando/cmd/log"
 	"rolando/cmd/repositories"
 	"rolando/cmd/utils"
-	"slices"
 	"sync"
+	"time"
 )
 
-// MediaStorage stores media URLs
+type MediaSet map[string]bool
+
 type MediaStorage struct {
 	chainID      string
-	gifs         map[string]struct{}
-	images       map[string]struct{}
-	videos       map[string]struct{}
+	gifs         MediaSet
+	images       MediaSet
+	videos       MediaSet
 	messagesRepo repositories.MessagesRepository
 	mu           sync.RWMutex
+	client       *http.Client
 }
 
 func NewMediaStorage(chainID string, gifs, images, videos []string, messagesRepo repositories.MessagesRepository) *MediaStorage {
-	storage := &MediaStorage{
-		chainID:      chainID,
-		gifs:         make(map[string]struct{}),
-		images:       make(map[string]struct{}),
-		videos:       make(map[string]struct{}),
-		messagesRepo: messagesRepo,
-	}
+	gifsMap := make(MediaSet, len(gifs))
+	imagesMap := make(MediaSet, len(images))
+	videosMap := make(MediaSet, len(videos))
+
 	for _, gif := range gifs {
-		storage.gifs[gif] = struct{}{}
+		gifsMap[gif] = true
 	}
 	for _, image := range images {
-		storage.images[image] = struct{}{}
+		imagesMap[image] = true
 	}
 	for _, video := range videos {
-		storage.videos[video] = struct{}{}
+		videosMap[video] = true
 	}
-	return storage
+
+	return &MediaStorage{
+		chainID:      chainID,
+		gifs:         gifsMap,
+		images:       imagesMap,
+		videos:       videosMap,
+		messagesRepo: messagesRepo,
+		client: &http.Client{
+			Timeout: 5 * time.Second,
+			Transport: &http.Transport{
+				MaxIdleConns:       100,
+				IdleConnTimeout:    90 * time.Second,
+				DisableCompression: true, // only need headers
+			},
+		},
+	}
 }
 
 func (ms *MediaStorage) AddMedia(url string) {
 	ms.mu.Lock()
-	defer ms.mu.Unlock()
 	if utils.IsGif(url) {
-		ms.gifs[url] = struct{}{}
+		ms.gifs[url] = true
 	} else if utils.IsVideo(url) {
-		ms.videos[url] = struct{}{}
+		ms.videos[url] = true
 	} else if utils.IsImage(url) {
-		ms.images[url] = struct{}{}
+		ms.images[url] = true
 	}
+	ms.mu.Unlock()
 }
 
 func (ms *MediaStorage) RemoveMedia(url string) {
 	ms.mu.Lock()
-	defer ms.mu.Unlock()
 	delete(ms.gifs, url)
 	delete(ms.videos, url)
 	delete(ms.images, url)
+	ms.mu.Unlock()
 }
 
 func (ms *MediaStorage) GetMedia(mediaType string) (string, error) {
 	ms.mu.RLock()
-
 	var urls []string
-	// Extract URLs based on the media type
+	var set MediaSet
+
 	switch mediaType {
 	case "gif":
-		for url := range ms.gifs {
-			urls = append(urls, url)
-		}
+		set = ms.gifs
 	case "image":
-		for url := range ms.images {
-			urls = append(urls, url)
-		}
+		set = ms.images
 	case "video":
-		for url := range ms.videos {
-			urls = append(urls, url)
-		}
+		set = ms.videos
 	default:
-		return "", errors.New("media type not found")
+		ms.mu.RUnlock()
+		return "", errors.New("invalid media type")
 	}
-	ms.mu.RUnlock()
-	// If no media found, return an error
-	if len(urls) == 0 {
+
+	if len(set) == 0 {
+		ms.mu.RUnlock()
 		return "", errors.New("no media found for this type")
 	}
 
-	// Try to get a valid URL
-	validURL, err := ms.getValidUrlFromSet(urls)
-	if err != nil {
-		return "", err
+	// Pre-allocate slice with exact size needed
+	urls = make([]string, 0, len(set))
+	for url := range set {
+		urls = append(urls, url)
 	}
+	ms.mu.RUnlock()
 
-	return validURL, nil
+	return ms.getValidUrlFromSet(urls)
 }
 
-// getValidUrlFromSet tries to return a valid URL from the set of URLs
 func (ms *MediaStorage) getValidUrlFromSet(urls []string) (string, error) {
-	for len(urls) > 0 {
-		randomIndex := rand.Intn(len(urls))
-		url := urls[randomIndex]
+	if len(urls) == 0 {
+		return "", errors.New("no URLs available")
+	}
 
-		// Validate the URL
+	// Create a copy of indices and shuffle them
+	indices := make([]int, len(urls))
+	for i := range indices {
+		indices[i] = i
+	}
+	rand.Shuffle(len(indices), func(i, j int) {
+		indices[i], indices[j] = indices[j], indices[i]
+	})
+
+	// Try URLs in random order
+	for _, idx := range indices {
+		url := urls[idx]
 		valid, err := ms.validateUrl(url)
 		if err != nil {
-			return ms.getValidUrlFromSet(slices.Concat(urls[:randomIndex], urls[randomIndex+1:]))
+			continue
 		}
-
 		if valid {
-			// Return the first valid URL
 			return url, nil
 		}
-
-		// If invalid, remove the URL from the set
-		urls = slices.Concat(urls[:randomIndex], urls[randomIndex+1:])
 	}
 
 	return "", errors.New("no valid media URLs found")
 }
 
-// validateUrl checks if the URL is valid (HTTP HEAD request)
 func (ms *MediaStorage) validateUrl(url string) (bool, error) {
-	// Make a HEAD request to check if the URL is valid
-	resp, err := http.Head(url)
+	req, err := http.NewRequest(http.MethodHead, url, nil)
 	if err != nil {
-		// On error, remove the invalid URL from the appropriate file
-		err = ms.removeInvalidUrl(url)
-		if err != nil {
-			return false, err
-		}
-		return false, nil
+		return false, err
 	}
 
-	// Check if the status code is 200 OK
+	resp, err := ms.client.Do(req)
+	if err != nil {
+		ms.handleInvalidUrl(url)
+		return false, nil
+	}
+	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
-		err = ms.removeInvalidUrl(url)
-		if err != nil {
-			log.Log.Errorf("Error removing invalid URL: %v", err)
-			return false, err
-		}
+		ms.handleInvalidUrl(url)
 		return false, nil
 	}
 
 	return true, nil
 }
 
-// removeInvalidUrl removes the invalid URL from DB messages
-func (ms *MediaStorage) removeInvalidUrl(url string) error {
+func (ms *MediaStorage) handleInvalidUrl(url string) {
 	ms.RemoveMedia(url)
-	return ms.messagesRepo.DeleteGuildMessagesContaining(ms.chainID, url)
+	if err := ms.messagesRepo.DeleteGuildMessagesContaining(ms.chainID, url); err != nil {
+		log.Log.Errorf("Error removing invalid URL: %v", err)
+	}
 }
