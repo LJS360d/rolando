615d81bea37eedf89b7a602b82d87e3b5541cd59
LJS360d <luca.lencioni09@gmail.com>
2025-01-19 08:54:04 +0100

    fix appPaginator import

--- client/src/components/AppPaginator.vue
+++ client/src/components/AppPaginator.vue
@@ -1,20 +1,10 @@
 <template>
   <div class="d-flex flex-wrap justify-center">
-    <v-select
-      v-model="pagination.pageSize"
-      :items="[10, 25, 50, 100, 250, 500, 1000]"
-      label="Page size"
-      style="width: min-content;"
-      class="w-min"
-    />
-    <v-pagination
-      v-if="pagination"
-      v-model="pagination.page"
-      :length="pagination.totalPages"
-      :total-visible="$vuetify.display.smAndDown ? 4 : 12"
-      prev-icon="fas fa-chevron-left"
-      next-icon="fas fa-chevron-right"
-    />
+    <v-select v-model="pagination.pageSize" :items="[10, 25, 50, 100, 250, 500, 1000]" label="Page size"
+      style="width: min-content;" class="w-min" />
+    <v-pagination v-if="pagination" v-model="pagination.page" :length="pagination.totalPages"
+      :total-visible="$vuetify.display.smAndDown ? 4 : 12" prev-icon="fas fa-chevron-left"
+      next-icon="fas fa-chevron-right" />
   </div>
 </template>
 
@@ -24,7 +14,7 @@ import { defineComponent } from "vue";
 export default defineComponent({
   props: {
     pagination: {
-      type: Object as () => import("@/api/data").PageMeta,
+      type: Object as () => import("@/api/common").PageMeta,
       required: true,
     },
   },
